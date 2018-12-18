cb99c794904b597bb78535e3dea738074e1becc6
noahiscool13 <noah@gamestoplay.nl>
2018-12-19 00:20:22 +0100

    TraceTest now plots from file

--- TraceTest.py
+++ TraceTest.py
@@ -6,8 +6,6 @@ with open("box.obj", "r") as file:
     triangles = scene.triangles
     lights = scene.lights
 
-#triangles = [Triangle(Vec3(0,5,-2),Vec3(-0.2,-5,-2), Vec3(0.2,-5,-2))]
-lights = [Light(Vec3(0, 0, -2), Vec3(1))]
 for x in triangles:
     print(x)
 print(lights[0])
